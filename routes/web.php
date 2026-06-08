aad59d9c508966e8a61c8bcf033e8e39b00e3899
https://github.com/adidkurniawan-wq <adidkurniawan@gmail.com>
2026-06-09 00:37:22 +0700

    Fix routes web.php and resolve route not found errors

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [ArticleController::class, 'index']);
+// Route untuk Halaman Utama & Pencarian (Poin 2)
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('/articles', [ArticleController::class, 'index']);
+
+// Route untuk Halaman Detail Artikel (Mengatasi eror articles.show)
+Route::get('/articles/{slug}', [ArticleController::class, 'show'])->name('articles.show');
